e88d8db76a2c576658d92f97e1e6e55c1c68adf3
Aryan Sinha <sinha.aryan03@gmail.com>
2021-05-16 18:19:50 +0530

    scripts: Add tiare Buildbot

---
+++ tiare.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
+git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common vendor/xiaomi/msm8937-common
+git clone https://github.com/mi-msm8937/android_device_xiaomi_tiare device/xiaomi/tiare
+git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_tiare vendor/xiaomi/tiare
