4d79b37e8cb72cf8c909fef9524f39c74865d54e
Aryan Sinha <sinha.aryan03@gmail.com>
2021-06-01 17:00:53 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+   sudo apt install bc git-core gnupg flex bison gperf  squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline6-dev gcc-multilib maven tmux screen w3m ncftp adb fastboot repo python default-jdk
