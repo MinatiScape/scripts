7a5c7205abb62a6f8ac9a1c558def36fa146dd1d
Aryan Sinha <sinha.aryan03@gmail.com>
2021-07-13 12:40:52 +0530

    Create exynos3475.sh

---
+++ exynos3475.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/Exynos3475/android_device_samsung_exynos3475-common -b cm-14.1 device/samsung/exynos3475-common
+git clone https://github.com/Exynos3475/android_device_samsung_o5prolte -b cm-14.1 device/samsung/o5prolte
+git clone https://github.com/Exynos3475/android_kernel_samsung_exynos3475 -b cm-14.1 kernel/samsung/exynos3475
+git clone https://github.com/Exynos3475/android_vendor_samsung -b cm-14.1 vendor/samsung
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-cm_exynos3475 -b cm-14.1 hardware/samsung_slsi-cm/exynos3475
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-cm_exynos -b cm-14.1 hardware/samsung_slsi-cm/exynos
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-cm_exynos5 -b cm-14.1 hardware/samsung_slsi-cm/exynos5
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi-cm_openmax -b cm-14.1 hardware/samsung_slsi-cm/openmax
+git clone https://github.com/LineageOS/android_hardware_samsung -b cm-14.1 hardware/samsung
+echo " Done !"
